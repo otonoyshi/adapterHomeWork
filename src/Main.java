453d5c109319ee4a680913e92b810baf4ab7298e
Georgy <georgy.matskevich@gmail.com>
2022-06-02 22:46:55 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        //демонстрация
+        System.out.println(calc.mult(2,2));
+        System.out.println(calc.pow(4,2));
+        System.out.println(calc.sum(12, 12));
+    }
+}
